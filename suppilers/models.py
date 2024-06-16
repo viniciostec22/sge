212c39dee034607703ad399a279910cb6f54f79d
vinicios <vinicios.tec22@gmail.com>
2024-06-16 13:49:28 -0300

    feat: setup, crud/brand/category

---
+++ suppilers/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['name']
+        
+        def __str__(self):
+            return self.name
