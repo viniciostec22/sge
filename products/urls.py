6c2f6754bb6cc4a92a6141ca89ac87236a5be326
vinicios <vinicios.tec22@gmail.com>
2024-06-20 14:47:10 -0300

    feat:crud_products

---
+++ products/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('product/list/', views.ProductListView.as_view(), name='product_list'),
+    path('product/create/', views.ProductCreateView.as_view(), name="product_create"),
+    path('product/<int:pk>/detail/', views.ProductDetailView.as_view(), name='product_datail'),
+    path('product/<int:pk>/update', views.ProductUpdateView.as_view(), name='product_update'),
+    path('product/<int:pk>/delete', views.ProductDeleteView.as_view(), name='product_delete'),
+]
