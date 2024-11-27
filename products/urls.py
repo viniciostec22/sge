9d1abc2aedaa436b81f689c89e93386cb66a73c0
vinicios <vinicios.tec22@gmail.com>
2024-11-27 15:37:51 -0300

    correção de names

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('product/list/', views.ProductListView.as_view(), name='product_list'),
     path('product/create/', views.ProductCreateView.as_view(), name="product_create"),
-    path('product/<int:pk>/detail/', views.ProductDetailView.as_view(), name='product_datail'),
+    path('product/<int:pk>/detail/', views.ProductDetailView.as_view(), name='product_detail'),
     path('product/<int:pk>/update', views.ProductUpdateView.as_view(), name='product_update'),
     path('product/<int:pk>/delete', views.ProductDeleteView.as_view(), name='product_delete'),
 ]
